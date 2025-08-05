13854af1dc4a2d05b4ae4e01d011ace0174aff05
new0628 <new970628@naver.com>
2025-08-05 12:10:07 +0900

    ios / open camera

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -13,6 +13,11 @@ import UIKit
     if let registrar = self.registrar(forPlugin: "OpenSetting") {
         OpenSetting.register(with: registrar)
     }
+    if let registrar = self.registrar(forPlugin: "OpenCamera") {
+        OpenCamera.register(with: registrar)
+    }
+
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
