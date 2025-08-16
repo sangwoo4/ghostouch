ae583a3fd7ea3401104a13741bc69748ca77cc0b
sangwoo4 <shangou97@gamil.com>
2025-08-16 21:28:23 +0900

    label sorted prevention

--- server/app/worker/ml/label_manager.py
+++ server/app/worker/ml/label_manager.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 from app.core import PathConfig
 
 
@@ -13,16 +13,20 @@ class LabelManager:
 
     def get_base_labels(self):
         y = self.basic_data[:, -1]
-        return list(np.unique(y))
+        return list(pd.unique(y)) # np.unique() 변경
 
     def get_combine_labels(self):
-        return np.unique(self.combine_data[:, -1])
-
+        y = self.combine_data[:, -1]
+        # CSV 등장 순서 보존
+        return list(pd.unique(y)) # np.unique() 변경
 
     def build_label_map(self):
         base_labels = self.get_base_labels()
         combine_labels = self.get_combine_labels()
-        new_labels = [label for label in combine_labels if label not in base_labels]
-        final_label_order = base_labels + list(new_labels)
+
+        base_set = set(base_labels)
+        new_labels = [l for l in combine_labels if l not in base_set]
+
+        final_label_order = base_labels + new_labels
         label_map = {label: i for i, label in enumerate(final_label_order)}
         return label_map, final_label_order
