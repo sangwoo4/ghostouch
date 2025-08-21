5bb5383506e50a5807bbef6450c6e4ad24c83990
sangwoo4 <138017980+sangwoo4@users.noreply.github.com>
2025-08-21 23:29:35 +0900

    Update celery_app.py

--- server/app/core/celery_app.py
+++ server/app/core/celery_app.py
@@ -2,8 +2,7 @@ from celery import Celery
 from app.utils import firebase_util
 
 
-# 도커로 실행할때
-# 로컬에서 실행할 때 주석 해제
+# run in local
 # celery_app = Celery(
 #     "worker",
 #     broker="redis://localhost:6379/0", # 메시지 큐
@@ -11,7 +10,7 @@ from app.utils import firebase_util
 #     include=['app.worker.training_tasks']
 # )
 
-#로컬로 실행
+# run in docker
 celery_app = Celery(
     "worker",
     broker="redis://host.docker.internal:6379/0", # 메시지 큐
